22567756cb893d995c6b3f60062def0355396925
Keir Lawson <keirlawson@gmail.com>
2015-05-14 09:14:46 +0100

    Add query string generation

--- src/civiPromise.js
+++ src/civiPromise.js
@@ -5,11 +5,40 @@ var civiPromise = {};
 
   var HTTP_GET = 'GET';
   var HTTP_POST = 'POST';
+  var STANDARD_ENDPOINT = '/sites/all/modules/civicrm/extern/rest.php';
 
   var civiDetails = {};
 
-  function composeUrl() {
-    
+  function generateQueryParam(params, key) {
+    var encodedKey = encodeURIComponent(key);
+    var encodedValue = encodeURIComponent(params[key]);
+    return encodedKey + '=' + encodedValue;
+  }
+
+  function generateQueryString(params) {
+    var generateFn = generateQueryParam.bind(undefined, params);
+    var queryString = Object.keys(params).map(generateFn);
+    return '?' + queryString;
+  }
+
+  function generateParams(entityType) {
+    var params = {
+      json: 1,
+      version: 3,
+      entity: entityType
+    };
+
+    //
+    //  key: civiDetails.siteKey,
+    //  api_key: civiDetails.apiKey,
+    //  action: action
+
+    return params;
+  }
+
+  function composeUrl(method, entityType) {
+    var queryString = generateQueryString(generateParams(entityType));
+    return civiDetails.siteLocation + STANDARD_ENDPOINT + queryString;
   }
 
   function queryCivi(method, entityType) {
